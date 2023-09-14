09b97d83f0ae71d6dc0ceae98a709863f91af0af
fikirtederibe <96678007+fikirtederibe@users.noreply.github.com>
2023-09-14 20:28:36 +0300

    Add files via upload

---
+++ Home Cinema/build/precache-manifest.f3c4d22463bd039b27b24c2056aea0e2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "e1e761186a0986fdb43f",
+    "url": "/Movie-App-React/static/css/main.d0691aff.chunk.css"
+  },
+  {
+    "revision": "e1e761186a0986fdb43f",
+    "url": "/Movie-App-React/static/js/main.e1e76118.chunk.js"
+  },
+  {
+    "revision": "a3ceb5115b40d5095f5e",
+    "url": "/Movie-App-React/static/js/1.a3ceb511.chunk.js"
+  },
+  {
+    "revision": "41f50b31996abe6251b5",
+    "url": "/Movie-App-React/static/js/runtime~main.41f50b31.js"
+  },
+  {
+    "revision": "4e9fc46446545f3f9659b3bd028b2c4a",
+    "url": "/Movie-App-React/static/media/Search.4e9fc464.svg"
+  },
+  {
+    "revision": "6b5b4ab756e0875f4d9ec5713f4092ab",
+    "url": "/Movie-App-React/index.html"
+  }
+];
